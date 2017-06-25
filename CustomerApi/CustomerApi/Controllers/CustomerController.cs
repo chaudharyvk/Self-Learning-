75aa163a8717120a3f3dd898658c082160158cc2
chaudharyvk <virendra.chaudhary2002@gmail.com>
2017-06-25 18:55:37 +0200

    Customer Web API to consume in Angular API
    Customer Web API to consume in Angular API
    Will Include Later to be deployed in Azure , logging , Exception
    handling , CRUS customer related information to database using entity
    framework and CQRS framework.

---
+++ CustomerApi/CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace CustomerApi.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        // GET: api/Customer
+        [JsonOutput]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Customer1", "Customer2" };
+        }
+
+        // GET: api/Customer/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customer
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Customer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class JsonOutputAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            ObjectContent content = actionExecutedContext.Response.Content as ObjectContent;
+            var value = content.Value;
+            Type targetType = actionExecutedContext.Response.Content.GetType().GetGenericArguments()[0];
+
+            var httpResponseMsg = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                RequestMessage = actionExecutedContext.Request,
+                Content = new ObjectContent(targetType, value, new JsonMediaTypeFormatter(), (string)null)
+            };
+
+            actionExecutedContext.Response = httpResponseMsg;
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
